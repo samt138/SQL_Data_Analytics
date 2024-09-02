0139a8cef9060c8160efa206c021a0ca955c6073
Sampath Kumar <sampathkumar008@gmail.com>
2024-09-02 11:39:58 +0530

    SQl_data

---
+++ project_sql/3_top_in_demand_skills.sql
@@ -0,0 +1,21 @@
+/*
+What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similart to previous query
+- Indentify the top 5 in-demand skills for a data analyst
+*/
+
+SELECT 
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM   
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE   
+    job_title_short = 'Data Analyst' AND
+    job_work_from_home = TRUE
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5
